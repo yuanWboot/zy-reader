78f8e3fe6d834549229e402a8d3c17237d0b5f90
zy <1661996964@qq.com>
2022-11-17 18:01:52 +0800

    添加事务控制

--- src/main/java/com/zy/reader/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/zy/reader/service/impl/CategoryServiceImpl.java
@@ -5,10 +5,13 @@ import com.zy.reader.entity.Category;
 import com.zy.reader.mapper.CategoryMapper;
 import com.zy.reader.service.CategoryService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 @Service
+@Transactional(propagation = Propagation.NOT_SUPPORTED,readOnly = true)
 public class CategoryServiceImpl implements CategoryService {
     @Resource
     private CategoryMapper categoryMapper;
