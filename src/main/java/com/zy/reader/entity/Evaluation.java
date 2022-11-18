0cccf31f65e84bc785f395fd55e8edaa735e9513
zy <1661996964@qq.com>
2022-11-18 11:03:04 +0800

    Evaluation实体类

---
+++ src/main/java/com/zy/reader/entity/Evaluation.java
@@ -0,0 +1,101 @@
+package com.zy.reader.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.util.Date;
+@TableName("evaluation")
+public class Evaluation {
+    @TableId(type = IdType.AUTO)
+    private Long evaluationId  ;
+    private String context ;
+    private Integer score  ;
+    private Date createTime;
+    private Long memberId  ;
+    private Long bookId  ;
+    private Integer enjoy  ;
+    private String state  ;
+    private String  disableReason  ;
+    private Date  disableTime  ;
+
+    public Long getEvaluationId() {
+        return evaluationId;
+    }
+
+    public void setEvaluationId(Long evaluationId) {
+        this.evaluationId = evaluationId;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getEnjoy() {
+        return enjoy;
+    }
+
+    public void setEnjoy(Integer enjoy) {
+        this.enjoy = enjoy;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDisableReason() {
+        return disableReason;
+    }
+
+    public void setDisableReason(String disableReason) {
+        this.disableReason = disableReason;
+    }
+
+    public Date getDisableTime() {
+        return disableTime;
+    }
+
+    public void setDisableTime(Date disableTime) {
+        this.disableTime = disableTime;
+    }
+}
