de0409d965edc660cabded4db0825fcc92117260
zy <1661996964@qq.com>
2022-11-20 13:59:10 +0800

    实现图片上传并回显

--- src/main/java/com/zy/reader/controller/management/MBookController.java
+++ src/main/java/com/zy/reader/controller/management/MBookController.java
@@ -3,11 +3,18 @@ package com.zy.reader.controller.management;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.zy.reader.service.BookService;
 import com.zy.reader.utils.ResponseUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/management/book")
@@ -31,5 +38,18 @@ public class MBookController {
             resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
         }
         return resp;
+    }
+    @PostMapping("/upload")
+    public Map upload(@RequestParam("img") MultipartFile file, HttpServletRequest request) throws IOException {
+        //得到文件上传目录
+        String uploadPath = request.getServletContext().getResource("/").getPath()+"/upload/";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String fileName = sdf.format(new Date());
+        String subffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        file.transferTo(new File(uploadPath+fileName+subffix));
+        Map result = new LinkedHashMap();
+        result.put("errno",0);
+        result.put("data",new String[]{"/upload/"+fileName+subffix});
+        return result;
     }
 }
